b2868890b242cab82d0a5084594d81b790bd3e90
Duncan Mac-Vicar P <dmacvicar@suse.de>
2012-02-24 01:42:46 +0100

    look for librpm.so.2 so that it works without librpm-dev too

--- lib/rpm/c.rb
+++ lib/rpm/c.rb
@@ -7,7 +7,7 @@ module RPM
     extend ::FFI::Library
 
     begin
-      ffi_lib('rpm')
+      ffi_lib ['rpm', 'librpm.so.2']
     rescue LoadError => e
       raise(
         "Can't find rpm libs on your system: #{e.message}"
