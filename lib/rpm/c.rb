df6a3fb084a5127bc34fc4dcc62dd3f09239ae2f
Duncan Mac-Vicar P <dmacvicar@suse.de>
2016-03-10 21:46:08 +0100

    search for librpm.so.3 too

--- lib/rpm/c.rb
+++ lib/rpm/c.rb
@@ -7,7 +7,7 @@ module RPM
     extend ::FFI::Library
 
     begin
-      ffi_lib ['rpm', 'librpm.so.2', 'librpm.so.1']
+      ffi_lib ['rpm', 'librpm.so.3', 'librpm.so.2', 'librpm.so.1']
     rescue LoadError => e
       raise(
         "Can't find rpm libs on your system: #{e.message}"
